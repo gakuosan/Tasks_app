f41ec4cd59f8862b60bcb6d893ef77d80cf821de
gakuchan <gakuo19831201@gmail.com>
2019-08-03 15:00:47 +0900

    Task機能修正 Gem追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,61 +1,57 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
 
-  def index
-    @tasks = Task.all.page(params[:page]).per(5)
+def index
+  @tasks = Task.all
+end
+
+def show
+  @task = Task.find(params[:id])
+end
+
+def new
+  @task = Task.new
+end
+
+def create
+   @task = Task.new(task_params)
+
+   if @task.save
+     flash[:success] = 'タスクが投稿されました'
+     redirect_to @task
+   else
+     flash.now[:danger] = 'タスクが投稿されませんでした'
+     render :new
+   end
+ end
+
+def edit
+  @task = Task.find(params[:id])
+end
+
+def update
+  @task = Task.find(params[:id])
+  if @task.update(task_params)
+    flash[:success] = 'タスクが編集されました'
+    redirect_to @task
+  else
+    flash.now[:danger] = 'タスクが編集されませんでした'
+    render :new
   end
-
-  def show
-    @task = Task.new
-    render "new"
-  end
+end
 
 
-  def new
-    @task = Task.new
-  end
+def destroy
+  @task = Task.find(params[:id])
+  @task.destroy
 
-  def create
-    @task = Task.new(task_params)
+  flash[:success] = 'タスクが削除されました'
+  redirect_to tasks_path
+end
 
-    if @task.save
-      flash[:success] = 'タスクが投稿されました'
-      redirect_to @task
-    else
-      flash.now[:danger] = 'タスクが投稿されませんでした'
-      render :new
-    end
-  end
+private
 
-  def edit
-  end
+def task_params
+  params.require(:task).permit(:content)
+end
 
-  def update
-    @task = Task.find(params[:id])
-
-    if @task.update(task_params)
-      flash[:success] = 'タスクが編集されました'
-      redirect_to @task
-    else
-      flash.now[:danger] = 'タスクが編集されませんでした'
-      render :new
-    end
-  end
-
-  def destroy
-    @task.destroy
-
-    flash[:success] = 'タスクが削除されました'
-    redirect_to tasks_path
-  end
-
-  private
-
-  def set_task
-    @task = Task.find(params[:id])
-  end
-
-  def task_params
-    params.require(:task).permit(:content, :status)
-  end
 end
