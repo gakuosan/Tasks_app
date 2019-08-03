2376df0c406317dcbe6a3454ae4dcb15edcf736b
gakuchan <gakuo19831201@gmail.com>
2019-08-03 16:18:20 +0900

    Bootstrapを使用

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,7 +5,7 @@ def index
 end
 
 def show
-  @task = Task.find(params[:id])
+  #@task = Task.find(params[:id])
 end
 
 def new
@@ -51,7 +51,7 @@ end
 private
 
 def task_params
-  params.require(:task).permit(:content)
+  params.require(:task).permit(:content )
 end
 
 end
