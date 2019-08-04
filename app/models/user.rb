1ff6c28281aef875bbb66644a94225f2fe56e8f5
gakuchan <gakuo19831201@gmail.com>
2019-08-04 10:46:30 +0900

    Login機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :tasks
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
   validates :name,  presence: true, length: { maximum: 30 }
